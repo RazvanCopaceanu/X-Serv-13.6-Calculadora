4d9db182b680025f49cedf9eea05f1bf6e0584f5
RazvanCopaceanu <razvan.copaceanu19@gmail.com>
2018-02-05 23:15:20 +0100

    calculadora v1

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) != 4:
+	sys.exit("python3 calculadora.py funcion operando1 operando2")
+_, funcion, operando1, operando2 = sys.argv
+
+if funcion == 'resta':
+	print(int(operando1) - int(operando2))
+elif funcion == 'suma':
+	print(int(operando1) + int(operando2))
+elif funcion == 'multiplicacion':
+	print(int(operando1) * int(operando2))
+elif funcion == 'division':
+	print(int(operando1) / int(operando2))
